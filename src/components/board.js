a8efac2bab623ce8301357bd1fdac98948c046b3
pgalkevich <pgalkevich@gmail.com>
2020-04-20 19:53:17 +0300

    module4-task2 ошибочный коммит. забыл переключиться

--- src/components/board.js
+++ src/components/board.js
@@ -1,31 +1,7 @@
-import {createElement} from "../utils.js";
+import AbstractComponent from "./abstract-component";
 
-
-const createBoardTemplate = () => {
-  return (
-    `<section class="board container"></section>`
-  );
-};
-
-
-export default class Board {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Board extends AbstractComponent {
   getTemplate() {
-    return createBoardTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+    return `<section class="board container"></section>`;
   }
 }
